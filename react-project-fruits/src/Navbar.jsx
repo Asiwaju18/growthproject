99f92b8c351eb27f03b6de5c6f33335c0746acb2
ABRAHAM2006 <ogunlakinasiwaju@gmail.com>
2026-01-27 14:59:42 +0100

    rep

--- react-project-fruits/src/Navbar.jsx
+++ react-project-fruits/src/Navbar.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <nav className="navbar">
-
+            {/* Logo */}
             <div className="nav-logo">
                 <img src="/assets/duvic-01 1.png" alt="Duvic Drop" />
             </div>
 
+            {/* Links */}
             <ul className="nav-links">
                 <li>About us</li>
+
+
+
                 <li>Contact us</li>
                 <li>Blog</li>
             </ul>
 
-
+            {/* Buttons */}
             <div className="nav-actions">
                 <button className="login-btn">Log in</button>
                 <button className="signup-btn">Sign up</button>
